f0ef43e366e1daa37d129b8cff7cf336e50ed9f1
Alex Mykyta <amykyta3@users.noreply.github.com>
2020-06-25 20:14:15 -0700

    Add import/export examples

---
+++ examples/README.md
@@ -0,0 +1,13 @@
+
+# Examples!
+
+## Convert a SystemRDL file to IP-XACT
+
+Use the `convert_to_ipxact.py` example script to translate SystemRDL into IP-XACT:
+
+```
+./convert_to_ipxact.py my_subblock.rdl
+```
+
+Results in the creation of the equivalent IP-XACT file: `my_subblock.xml`
+
---
+++ examples/convert_to_ipxact.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+from systemrdl import RDLCompiler, RDLCompileError
+from peakrdl.ipxact import IPXACTExporter
+
+# Collect input files from the command line arguments
+input_files = sys.argv[1:]
+
+# Create an instance of the compiler
+rdlc = RDLCompiler()
+
+try:
+    # Compile all the files provided
+    for input_file in input_files:
+        rdlc.compile_file(input_file)
+    
+    # Elaborate the design
+    root = rdlc.elaborate()
+except RDLCompileError:
+    # A compilation error occurred. Exit with error code
+    sys.exit(1)
+
+
+exporter = IPXACTExporter()
+exporter.export(root.top, "%s.xml" % root.top.inst_name)
